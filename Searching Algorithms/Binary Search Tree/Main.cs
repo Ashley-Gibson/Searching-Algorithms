5ce45cc40a856769fac5adacd114ee3cf26fe229
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-05-27 20:41:28 +0100

    task/SA-168 - Documentation of resources used for the BST

--- Searching Algorithms/Binary Search Tree/Main.cs
+++ Searching Algorithms/Binary Search Tree/Main.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Resources: 
+// https://youtu.be/ZNH0MuQ51m4?list=PLO3CT4tUbboPe9EgndTUcNEpPzg-K1pj0
+// https://en.wikipedia.org/wiki/Binary_search_tree
 
 namespace Binary_Search_Tree
 {
